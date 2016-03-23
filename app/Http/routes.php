b2e29a16a8c4ed62afc651cb1c1f7611920e7c5e
Khuong Chu Nguyen <Khuong Chu Nguyen>
2016-03-23 21:36:11 +0200

    Test Laravelmessenger succesfully

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -59,6 +59,8 @@ Route::get('login/fb/callback', function() {
 
     return Redirect::to('/')->with('message', 'Logged in with Facebook');
 });
+
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -74,4 +76,12 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
+
+    Route::group(['prefix' => 'messages'], function () {
+    Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+    Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
+    Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
+    Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+    Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
+});
 });
